e5ba7f4b75518831a38cb2792ad80427c92e1ce8
Cody Dennis <codyjdennis@gmail.com>
2017-10-28 17:05:11 -0700

    Assignment 24 Album View: completed

--- scripts/album.js
+++ scripts/album.js
@@ -28,6 +28,21 @@ var albumPicasso = {
       ]
   };
 
+  var albumMajid = {
+       title: 'The Space Between',
+       artist: 'Majid Jordan',
+       label: 'OVO Sound',
+       year: '2017',
+       albumArtUrl: 'assets/images/album_covers/30.png',
+       songs: [
+           { title: 'Intro', duration: '1:01' },
+           { title: 'Gave Your Love Away', duration: '5:01' },
+           { title: 'OG Heartthrob', duration: '3:21'},
+           { title: 'Body Talk', duration: '3:14' },
+           { title: 'Not Ashamed', duration: '2:15'}
+       ]
+   };
+
   var createSongRow = function(songNumber, songName, songLength) {
      var template =
         '<tr class="album-view-song-item">'
@@ -40,14 +55,13 @@ var albumPicasso = {
      return template;
  };
 
- var setCurrentAlbum = function(album) {
-     // #1
-     var albumTitle = document.getElementsByClassName('album-view-title')[0];
-     var albumArtist = document.getElementsByClassName('album-view-artist')[0];
-     var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
-     var albumImage = document.getElementsByClassName('album-cover-art')[0];
-     var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+ var albumTitle = document.getElementsByClassName('album-view-title')[0];
+ var albumArtist = document.getElementsByClassName('album-view-artist')[0];
+ var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
+ var albumImage = document.getElementsByClassName('album-cover-art')[0];
+ var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
 
+ var setCurrentAlbum = function(album) {
      // #2
      albumTitle.firstChild.nodeValue = album.title;
      albumArtist.firstChild.nodeValue = album.artist;
@@ -65,4 +79,14 @@ var albumPicasso = {
 
  window.onload = function() {
      setCurrentAlbum(albumPicasso);
+
+ var albums = [albumPicasso, albumMarconi, albumMajid];
+ var index = 1;
+ albumImage.addEventListener("click", function(event) {
+   setCurrentAlbum(albums[index]);
+   index++;
+   if (index == albums.length) {
+     index = 0;
+   }
+    });
  };
