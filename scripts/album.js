360a7d39758abac903d1893bda4dac3de5fd040e
Cody Dennis <codyjdennis@gmail.com>
2017-10-30 19:51:14 -0700

    Checkpoint 25 Play/Pause Pt1: completed

--- scripts/album.js
+++ scripts/album.js
@@ -28,10 +28,25 @@ var albumPicasso = {
       ]
   };
 
+  var albumMajid = {
+       title: 'The Space Between',
+       artist: 'Majid Jordan',
+       label: 'OVO Sound',
+       year: '2017',
+       albumArtUrl: 'assets/images/album_covers/30.png',
+       songs: [
+           { title: 'Intro', duration: '1:01' },
+           { title: 'Gave Your Love Away', duration: '5:01' },
+           { title: 'OG Heartthrob', duration: '3:21'},
+           { title: 'Body Talk', duration: '3:14' },
+           { title: 'Not Ashamed', duration: '2:15'}
+       ]
+   };
+
   var createSongRow = function(songNumber, songName, songLength) {
      var template =
         '<tr class="album-view-song-item">'
-      + '  <td class="song-item-number">' + songNumber + '</td>'
+      + '  <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
       + '  <td class="song-item-title">' + songName + '</td>'
       + '  <td class="song-item-duration">' + songLength + '</td>'
       + '</tr>'
@@ -40,14 +55,13 @@ var albumPicasso = {
      return template;
  };
 
- var setCurrentAlbum = function(album) {
-     // #1
-     var albumTitle = document.getElementsByClassName('album-view-title')[0];
-     var albumArtist = document.getElementsByClassName('album-view-artist')[0];
-     var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
-     var albumImage = document.getElementsByClassName('album-cover-art')[0];
-     var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+ var albumTitle = document.getElementsByClassName('album-view-title')[0];
+ var albumArtist = document.getElementsByClassName('album-view-artist')[0];
+ var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
+ var albumImage = document.getElementsByClassName('album-cover-art')[0];
+ var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
 
+ var setCurrentAlbum = function(album) {
      // #2
      albumTitle.firstChild.nodeValue = album.title;
      albumArtist.firstChild.nodeValue = album.artist;
@@ -63,6 +77,33 @@ var albumPicasso = {
      }
  };
 
+var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
+var songRows = document.getElementsByClassName('album-view-song-item');
+
+ var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
+
  window.onload = function() {
      setCurrentAlbum(albumPicasso);
+
+     songListContainer.addEventListener('mouseover', function(event) {
+       if (event.target.parentElement.className === 'album-view-song-item') {
+         event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
+     }
+ });
+
+ for (var i = 0; i < songRows.length; i++) {
+        songRows[i].addEventListener('mouseleave', function(event) {
+          this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
+       });
+   }
+
+ var albums = [albumPicasso, albumMarconi, albumMajid];
+ var index = 1;
+ albumImage.addEventListener("click", function(event) {
+   setCurrentAlbum(albums[index]);
+   index++;
+   if (index == albums.length) {
+     index = 0;
+   }
+    });
  };
