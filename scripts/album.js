ffe9cbe46543a90e9bb9192bcdda1f653ab3ff2a
Cody Dennis <codyjdennis@gmail.com>
2017-11-12 17:10:42 -0800

    Assignment 32 Buzz Library: Completed

--- scripts/album.js
+++ scripts/album.js
@@ -176,6 +176,20 @@ var updatePlayerBarSong = function() {
     $('.main-controls .play-pause').html(playerBarPauseButton);
 };
 
+var togglePlayFromPlayerBar = function() {
+     if (currentSoundFile.isPaused()) {
+         $(this).html(pauseButtonTemplate);
+         playerBarActions.html(playerBarPauseButton);
+         currentSoundFile.play();
+     } else {
+         $(this).html(playButtonTemplate);
+         playerBarActions.html(playerBarPlayButton);
+         currentSoundFile.pause();
+     }
+ };
+
+
+
 // Album button templates
 var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
 var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
@@ -192,10 +206,12 @@ var currentVolume = 80;
 
 var $previousButton = $('.main-controls .previous');
 var $nextButton = $('.main-controls .next');
+var playerBarActions = $('.main-controls .play-pause');
 
 
 $(document).ready(function() {
     setCurrentAlbum(albumPicasso);
     $previousButton.click(previousSong);
     $nextButton.click(nextSong);
+    playerBarActions.click(togglePlayFromPlayerBar);
 });
